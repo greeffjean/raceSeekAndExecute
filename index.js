52fe866c822259fe241cab6388a88088842654fa
Jean <jeang@nml.co.za>
2023-08-25 13:54:14 +0200

    add conditional fn execution logic

--- index.js
+++ index.js
@@ -1,25 +1,37 @@
 
+const queryNotFoundMsg = "Your seek query timed-out!";
+
 /** 
 ### Objective - Seek and execute
 
 Props: 
-- boolean: any value that you want to seek, 
-- callback: callback function to execute once `boolean` has been validated as a truthy value, 
+- query: query string - (A string containing one or more selectors to match. This string must be a valid CSS selector string), 
+- callback: callback function to execute once `query` has been validated as a truthy value, 
 - interval: interval in which your seeking process will occur, defaults to `1000ms`, 
 - timeout: timeout that determines when your `setInterval` loop will terminate, defaults to `10000ms`
 */
-const raceSeekAndExecute = (boolean, callback, interval = 1000, timeout = 10000) => {
-    
-    const _interval = setInterval(() => {
-        if (boolean) {
+const raceSeekAndExecute = (
+    query,
+    callback,
+    interval = 1000,
+    timeout = 10000
+) => {
+    let _interval;
+    let _timeout;
+
+    _interval = setInterval(() => {
+        const element = document.querySelector(query);
+        if (element) {
             if (typeof callback === "function") callback();
             clearInterval(_interval);
+            clearTimeout(_timeout);
         }
     }, interval);
 
-    setTimeout(() => {
+    _timeout = setTimeout(() => {
+        console.error(queryNotFoundMsg);
         clearInterval(_interval);
-    }, timeout)
+    }, timeout);
 };
 
 export { raceSeekAndExecute };
